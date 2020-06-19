089b7c7c43331e39b0ceb7f892390860a3dd5b6d
Ihor Honcharenko <ihorhoncharenko8@gmail.com>
2020-06-19 19:00:58 +0300

    Updates

---
+++ precache-manifest.78544fb900cffe786c1c530cb03d85e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd2ca479a1c3fe637ca53fc2e1c5c62",
+    "url": "/sport-clubs-test-task/index.html"
+  },
+  {
+    "revision": "587b78974f5a76a71583",
+    "url": "/sport-clubs-test-task/static/css/main.662066d6.chunk.css"
+  },
+  {
+    "revision": "ca35aa4c4b7b31ce2a62",
+    "url": "/sport-clubs-test-task/static/js/2.a95c4912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sport-clubs-test-task/static/js/2.a95c4912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587b78974f5a76a71583",
+    "url": "/sport-clubs-test-task/static/js/main.75007379.chunk.js"
+  },
+  {
+    "revision": "ab794aeed16d1bda5c2a",
+    "url": "/sport-clubs-test-task/static/js/runtime-main.df5a17a6.js"
+  }
+]);
